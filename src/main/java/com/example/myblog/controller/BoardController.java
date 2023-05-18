fec026e4f248b4f4dfe3ec8c7bdae462bbe3c08c
imjaeyeol <imjy2@naver.com>
2023-05-18 16:23:47 +0900

    20230518 first commit

--- src/main/java/com/example/myblog/controller/BoardController.java
+++ src/main/java/com/example/myblog/controller/BoardController.java
@@ -1,5 +1,45 @@
 package com.example.myblog.controller;
 
+
+import com.example.myblog.model.Board;
+import com.example.myblog.service.BoardService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+
 public class BoardController {
+    private final BoardService boardService;
+
+    //전체글 보기
+    @GetMapping({"", "/"})
+    public String index(Model model, @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC)Pageable pageable){
+        Page<Board> board = boardService.list(pageable);
+        model.addAttribute("boars", board);
+       return "index";
+    }
+
+    @GetMapping("/board/daily")
+    public String dailyList(Model model, @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<Board> daily = boardService.categoryList(pageable, "daily");
+        model.addAttribute("boards", daily);
+        return "category/daily";
+    }
+
+    @GetMapping("/board/knowledge")
+    public String KnowledgeList(Model model, @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<Board> knowledge = boardService.categoryList(pageable, "knowledge");
+        model.addAttribute("boards", knowledge);
+        return "category/knowledge";
+    }
 
 }
